c9454249512e149aab8aeac809bcec312c174058
JesusContreras26 <jgcc26@gmail.com>
2024-03-09 23:32:25 -0400

    individual 2

--- src/js/product.js
+++ src/js/product.js
@@ -3,7 +3,7 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParams('product');
+const productId = getParams("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -39,3 +39,13 @@ export function getParams(param){
   const product = urlParams.get(param);
   return product;
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false ){
+  const htmlStrings = list.map(templateFn);
+
+  if (clear){
+    parentElement.innerHTML = "";
+  }
+
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
