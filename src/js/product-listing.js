0d1db75fbf28cc9a4e2fb7f7b6be34037484ca8b
JesusContreras26 <jgcc26@gmail.com>
2024-03-22 20:15:13 -0400

    team activity week 4

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,11 +1,11 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductListing from "./ProductListing.mjs";
 import { loadHeaderFooter , getParams } from "./utils.mjs";
 
 loadHeaderFooter();
 
 const param = getParams("product");
-const dataS = new ProductData();
+const dataS = new ExternalServices();
 const htmlList = document.querySelector(".product-list"); 
 const productList = new ProductListing(param, dataS, htmlList);
 
