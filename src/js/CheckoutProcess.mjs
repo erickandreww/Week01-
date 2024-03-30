2bedbc37c2b305913d618a05abb1a8dca0a8f23b
erickandreww <erick.andrew.moura@gmail.com>
2024-03-30 15:28:50 -0300

    "team actvity 4 - almost done"

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,5 +1,27 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, alertMessage, removeAlerts } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 
+const extServices = new ExternalServices();
+
+function packageItems(items) {
+    const simpleItems = items.map((product) => {
+        return {
+            id: product.Id,
+            price: product.FinalPrice, 
+            name: product.Name, 
+            quantity: 1,
+        };
+    });
+    return simpleItems;
+}
+
+function formDataToJSON(formElement) {
+    const formData = new FormData(formElement), convertedJSON = {};
+    formData.forEach(function (value, key) {
+      convertedJSON[key] = value;
+    });
+    return convertedJSON;
+}
 
 export default class CheckoutProcess {
     constructor(key, outputSelector) {
@@ -18,8 +40,8 @@ export default class CheckoutProcess {
     }
     
     calculateItemSummary() {
-        const summaryElement = document.querySelector(this.outputSelector + " #cartTotal");
-        const itemNumElement = document.querySelector(this.outputSelector + " #num-items");
+        const itemNumElement = document.querySelector(this.output + " #total-items");
+        const summaryElement = document.querySelector(this.output + " #cartTotal");
         itemNumElement.innerText = this.list.length;
         // calculate the total of all the items in the cart
         const amounts = this.list.map((item) => item.FinalPrice);
@@ -30,20 +52,37 @@ export default class CheckoutProcess {
     calculateOrderTotal() {
         this.shipping = 10 + (this.list.length - 1) * 2;
         this.tax = (this.itemTotal * 0.06).toFixed(2);
-        this.orderTotal = (
-            parseFloat(this.itemTotal) +
-            parseFloat(this.shipping) +
-            parseFloat(this.tax)
-          ).toFixed(2);
+        this.orderTotal = (parseFloat(this.itemTotal) + parseFloat(this.shipping) +
+            parseFloat(this.tax)).toFixed(2);
         this.displayOrderTotals();
     }
 
     displayOrderTotals() {
-        const shippingElement = document.querySelector("#shipping");
-        const taxElement = document.querySelector("#tax");
-        const orderTotalElement = document.querySelector("#orderTotal");
-        shippingElement.innerText = "$ " + this.shipping;
-        taxElement.innerText = "$ " + this.tax;
-        orderTotalElement.innerText = "$ " + this.orderTotal;
+        const shipping = document.querySelector(this.outputSelector + " #shipping");
+        const tax = document.querySelector(this.outputSelector + " #tax");
+        const orderTotal = document.querySelector(
+          this.outputSelector + " #orderTotal"
+        );
+        shipping.innerText = "$" + this.shipping;
+        tax.innerText = "$" + this.tax;
+        orderTotal.innerText = "$" + this.orderTotal;
+    }
+
+    async checkout() {
+        const formElement = document.forms["checkout"];
+        const json = formDataToJSON(formElement);
+        json.orderData = new Date();
+        json.orderTotal = this.orderTotal;
+        json.tax = this.tax;
+        json.shipping = this.shipping;
+        json.items = packageItems(this.list);
+        console.log(json);
+        try {
+            const res = await extServices.checkout(json);
+            console.log(res);
+        } 
+        catch (err) {
+            console.log(err);
+        }
     }
 }
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,12 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+const checking  = new CheckoutProcess("so-cart", ".orderSummary");
+checking.init();
+
+document.querySelector("#zip").addEventListener("blur", myCheckout.calculateOrdertotal.bind(checking));
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+    e.preventDefault();
+    checking.checkout();
+});
